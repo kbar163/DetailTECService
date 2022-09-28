820e8c049b4d565bb6a36066cfb26b1c962bcd19
kbar163 <kabarrantes163@gmail.com>
2022-09-27 18:27:04 -0600

    Creacion de proyecto webapi .NET

---
+++ Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Middleware utilizado para habilitar politicas de CORS en los endpoints del REST API.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Policy",
+    policy =>
+    {
+        policy
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
