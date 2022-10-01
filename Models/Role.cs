cb5ac72c176e4764d12eb6db178350644d866db0
kbar163 <kabarrantes163@gmail.com>
2022-09-30 20:42:55 -0600

    Creacion de modelos de datos
    * Se crea el modelo de datos Role para los roles de los empleados.
    * Se crea el modelo de datos MultivalueRole para enviar respuesta de GET multivalor.

---
+++ Models/Role.cs
@@ -0,0 +1,10 @@
+namespace DetailTECService.Models
+{
+    //Modelo de datos que representa una entrada en la 
+    //tabla ROL en la base de datos.
+    public class Role
+    {
+        public int id_rol { get; set; }
+        public string? tipo_rol { get; set; }
+    }
+}
