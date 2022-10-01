0dcf83c885cf7bd225963a6cb3e44e3f51903132
kbar163 <kabarrantes163@gmail.com>
2022-09-30 20:22:09 -0600

    Bugfix, formato de values en DB
    Se agregan mayusculas a nombres, apellidos y roles en el script loadTestUsers.sql

--- Data/Scripts/loadTestUsers.sql
+++ Data/Scripts/loadTestUsers.sql
@@ -1,24 +1,24 @@
 INSERT INTO ROL
 VALUES
-(0, 'gerente'),
-(1, 'lavador'),
-(2, 'pulidor');
+(0, 'Gerente'),
+(1, 'Lavador'),
+(2, 'Pulidor');
 
 INSERT INTO TIPO_PAGO
 VALUES
-(0, 'semanal'),
-(1, 'bisemanal');
+(0, 'Semanal'),
+(1, 'Bisemanal');
 
 INSERT INTO CLIENTE
 VALUES 
-('409837345','erick', 'barrantes', 'cerdas', 'correo01@gmail.com', 'user01', 'password01',9),
-('709837345','manuel', 'brenes', 'salazar', 'correo02@gmail.com', 'user02', 'password02',0),
-('609837345','sofia', 'ramirez', 'gomez', 'correo03@gmail.com', 'user03', 'password03',5),
-('209837345','veronica', 'sandoval', 'sanchez', 'correo04@gmail.com', 'user04', 'password04',12);
+('409837345','Erick', 'Barrantes', 'Cerdas', 'correo01@gmail.com', 'user01', 'password01',9),
+('709837345','Manuel', 'Brenes', 'Salazar', 'correo02@gmail.com', 'user02', 'password02',0),
+('609837345','Sofia', 'Ramirez', 'Gomez', 'correo03@gmail.com', 'user03', 'password03',5),
+('209837345','Veronica', 'Sandoval', 'Sanchez', 'correo04@gmail.com', 'user04', 'password04',12);
 
 INSERT INTO TRABAJADOR
 VALUES
-('303450213', '1999-12-07', 'erick', 'barrantes', 'cerdas', 0,1,'2020-01-01','password05'),
-('102340567', '1999-12-07', 'jose', 'ramos', 'rojas', 1,1,'2020-01-01','password06'),
-('207650385', '1999-12-07', 'maria', 'nunes', 'salguero', 1,1,'2020-01-01','password07'),
-('408730769', '1999-12-07', 'brienne', 'mejia', 'lopez', 1,1,'2020-01-01','password08');
+('303450213', '1999-12-07', 'Erick', 'Barrantes', 'Cerdas', 0,1,'2020-01-01','password05'),
+('102340567', '1999-12-07', 'Jose', 'Ramos', 'Rojas', 1,1,'2020-01-01','password06'),
+('207650385', '1999-12-07', 'Maria', 'Nunes', 'Salguero', 1,1,'2020-01-01','password07'),
+('408730769', '1999-12-07', 'Brienne', 'Mejia', 'Lopez', 1,1,'2020-01-01','password08');
