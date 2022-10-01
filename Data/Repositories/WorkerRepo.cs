e5cbabfc6b9a4d361343418e1d62c294f825eede
kbar163 <kabarrantes163@gmail.com>
2022-09-30 20:54:05 -0600

    Creacion de repositorio para logica de trabajadores
    * Se crea la interfaz IWorkerRepo que declara las funciones de logica de manejo de trabajadores.
    * Se crea la clase WorkerRepo que implementa la interfaz.
    * Se agrega en Program.cs la declaracion de scoped service que mapea la inrerfaz a la clase de implementacion.

---
+++ Data/Repositories/WorkerRepo.cs
@@ -0,0 +1,24 @@
+using System.Data;
+using DetailTECService.Models;
+using Microsoft.Data.SqlClient;
+
+
+namespace DetailTECService.Data
+{
+    public class WorkerRepo : IWorkerRepository
+    {
+        private readonly string _connectionString;
+
+        public WorkerRepo()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _connectionString = config.GetValue<string>("ConnectionStrings:DetailTECDB");
+
+        }
+        
+        public AuthResponse GetRoles()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Models/MultivalueRoles.cs
+++ Models/MultivalueRoles.cs
@@ -2,7 +2,7 @@ namespace DetailTECService.Models
 {
 
     //Esta clase es usada como modelo de datos para respuesta de GET request
-    //en el que se devuelve una lista de valores del modelo Role
+    //en el que se devuelve una lista de valores del modelo Role.
     public class MultivalueRole
     {
         public bool exito { get; set; }
