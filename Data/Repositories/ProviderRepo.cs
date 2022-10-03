78e2a4d37febd4c075e38ac111ce993c9ddad57b
Jose Julian <juliansolqu18@gmail.com>
2022-10-02 20:48:41 -0600

    [P1-15] Agrega endpoints de agregar, editar y eliminar proveedores

--- Data/Repositories/ProviderRepo.cs
+++ Data/Repositories/ProviderRepo.cs
@@ -24,17 +24,8 @@ namespace DetailTECService.Data
         //Salida: DataTable data con la informacion solicitada en el query de ser exitoso,
         //DataTable data vacio en caso de que el query no fuese exitoso.
  
-                public MultivalueProvider GetAllProviders()
-        {
-            MultivalueProvider response;
-            string query = @"SELECT PROVEEDOR.CEDULA_JURIDICA_PROVEEDOR, PROVEEDOR.NOMBRE, PROVEEDOR.CONTACTO, 
-             PROVEEDOR.PROVINCIA, PROVEEDOR.CANTON, PROVEEDOR.DISTRITO, PROVEEDOR.CORREO_ELECTRONICO 
-             FROM PROVEEDOR";
-            DataTable dbTable = GetTableData(query);
-            response = AllProvidersMessage(dbTable);
-            return response;
-        }
-               private DataTable GetTableData(string query)
+
+        private DataTable GetTableData(string query)
         {
             var data = new DataTable();
         
@@ -64,8 +55,17 @@ namespace DetailTECService.Data
             }       
             return data;
         }
-   
 
+        public MultivalueProvider GetAllProviders()
+        {
+            MultivalueProvider response;
+            string query = @"SELECT PROVEEDOR.CEDULA_JURIDICA_PROVEEDOR, PROVEEDOR.NOMBRE, PROVEEDOR.CONTACTO, 
+             PROVEEDOR.PROVINCIA, PROVEEDOR.CANTON, PROVEEDOR.DISTRITO, PROVEEDOR.CORREO_ELECTRONICO 
+             FROM PROVEEDOR";
+            DataTable dbTable = GetTableData(query);
+            response = AllProvidersMessage(dbTable);
+            return response;
+        }
         //Entradas: 
         //DataTable dbTable: DataTable que potencialmente contiene informacion obtenida de la base de datos.
         //Proceso: Se revisa si dbTable tiene contenido, de ser asi, se cambia la propiedad boolean de la respuesta
@@ -101,6 +101,142 @@ namespace DetailTECService.Data
             return response;
         }
 
+        //Proceso: 
+        //Intenta conectarse a la base de datos haciendo uso de un SqlConnection,
+        //Intenta ejecutar INSERT o UPDATE sobre la base de datos en la tabla PROVEEDOR
+        //Salida: ActionResponse response: un objeto que tiene una propiedad booleana que indica si la 
+        //operacion fue exitosa o no, y una propiedad message con un string que describe el resultado de
+        //la operacion.
+        public ActionResponse WriteProviderDB(string query, Provider newProvider)
+        {
+            ActionResponse response = new ActionResponse();
+            string verb = "";
+            string infinitive = "";
 
+            try
+            {
+                if (query.Contains("INSERT"))
+                {
+                    verb = "creado";
+                    infinitive = "crear";
+                }
+
+                if (query.Contains("UPDATE"))
+                {
+                    verb = "actualizado";
+                    infinitive = "actualizar";
+                }
+                
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.Add(new SqlParameter("@cedula_juridica_proveedor", newProvider.cedula_juridica_proveedor));
+                        command.Parameters.Add(new SqlParameter("@nombre", newProvider.nombre));
+                        command.Parameters.Add(new SqlParameter("@telefono", newProvider.telefono));
+                        command.Parameters.Add(new SqlParameter("@provincia", newProvider.provincia));
+                        command.Parameters.Add(new SqlParameter("@canton", newProvider.canton));
+                        command.Parameters.Add(new SqlParameter("@distrito", newProvider.distrito));
+                        command.Parameters.Add(new SqlParameter("@correo_electronico", newProvider.correo_electronico));
+                        connection.Open();
+                        Console.WriteLine("Connection to DB stablished");
+                        command.ExecuteNonQuery();    
+                        response.actualizado = true;
+                        response.mensaje = $"Proveedor {verb} exitosamente";
+
+                    } 
+                }   
+            }
+            catch (Exception ex)
+            {
+                if(ex is ArgumentException ||
+                   ex is SqlException || ex is InvalidOperationException)
+                {
+                    Console.WriteLine("ERROR: " + ex.Message +  "triggered by " + ex.Source);
+                    response.actualizado = false;
+                    response.mensaje = $"Error al {infinitive} al proveedor";
+                }
+            }
+            return response;
+        }
+
+        //Proceso: Punto de entrada del proceso de crear un proveedor, hace uso de una funcion
+        //auxiliar que inserta informacion a la base de datos. 
+        //Salida: ActionResponse response: un objeto que tiene una propiedad booleana que indica si la 
+        //operacion fue exitosa o no, y una propiedad message con un string que describe el resultado de
+        //la operacion.
+        public ActionResponse AddProvider(Provider newProvider)
+        {
+            ActionResponse response;
+            string query = @"INSERT INTO PROVEEDOR
+            VALUES (@cedula_juridica_proveedor , @nombre , @telefono ,
+            @provincia , @canton , @distrito , @correo_electronico)";
+            response = WriteProviderDB(query, newProvider);
+            return response;
+        }
+
+        //Proceso: Punto de entrada del proceso de modificar un proveedor, hace uso de una funcion
+        //auxiliar que inserta informacion a la base de datos. 
+        //Salida: ActionResponse response: un objeto que tiene una propiedad booleana que indica si la 
+        //operacion fue exitosa o no, y una propiedad message con un string que describe el resultado de
+        //la operacion.
+        public ActionResponse ModifyProvider(Provider newProvider)
+        {
+            ActionResponse response;
+            string query = @"UPDATE PROVEEDOR
+            SET NOMBRE= @nombre,
+            CONTACTO = @telefono ,
+            PROVINCIA = @provincia ,
+            CANTON = @canton ,
+            DISTRITO = @distrito ,
+            CORREO_ELECTRONICO = @correo_electronico 
+            WHERE CEDULA_JURIDICA_PROVEEDOR = @cedula_juridica_proveedor";
+            response = WriteProviderDB(query, newProvider);
+            return response;
+        }
+
+        //Entrada: ProviderIdRequest deleteId, tiene una propiedad string que representa la cedula juridica de un proveedor
+        //Proceso: Se crea un query para eliminar de la DB al proveedorcuyo CEDULA__JURIDICA_PROVEEDOR haga match con
+        //la propiedad cedula_juridica_proveedor de deleteId.
+        //Intenta conectarse a la base de datos haciendo uso de un SqlConnection,
+        //Intenta ejecutar DELETE sobre la base de datos en la tabla PROVEEDOR
+        //Salida: ActionResponse response: un objeto que tiene una propiedad booleana que indica si la 
+        //operacion fue exitosa o no, y una propiedad message con un string que describe el resultado de
+        //la operacion.
+        public ActionResponse DeleteProvider(ProviderIdRequest deleteId)
+        {
+            ActionResponse response = new ActionResponse();
+            string query = @"DELETE FROM PROVEEDOR
+            WHERE CEDULA_JURIDICA_PROVEEDOR = @cedula_juridica_proveedor";
+            
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.Add(new SqlParameter("@cedula_juridica_proveedor", deleteId.cedula_juridica_proveedor));
+                        connection.Open();
+                        Console.WriteLine("Connection to DB stablished");
+                        command.ExecuteNonQuery();
+                        response.actualizado = true;
+                        response.mensaje = "Proveedor eliminado exitosamente";
+                        
+                    } 
+                }   
+            }
+
+            catch (Exception ex)
+            {
+                if(ex is ArgumentException ||
+                   ex is SqlException || ex is InvalidOperationException)
+                {
+                    Console.WriteLine("ERROR: " + ex.Message +  "triggered by " + ex.Source);
+                    response.actualizado = false;
+                    response.mensaje = "Error al eliminar proveedor";
+                }
+            }
+            return response;
+        }
     }
 }
