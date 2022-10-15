57ac6e71b8ac95e1812db825cc89e23d66cfa527
kbar163 <kabarrantes163@gmail.com>
2022-10-15 00:58:40 -0600

    WIP
    Creado reporte tipo 2

--- Data/Repositories/ReportRepo.cs
+++ Data/Repositories/ReportRepo.cs
@@ -49,7 +49,7 @@ namespace DetailTECService.Data
 
         private void PointReport()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Not implemented");
         }
 
         private void WashReport(string cedula)
@@ -58,27 +58,66 @@ namespace DetailTECService.Data
             CITA.NOMBRE_LAVADO
             FROM CITA
             WHERE CITA.CEDULA_CLIENTE = @id
-            AND CITA.FACTURADA = TRUE";
+            AND CITA.FACTURADA = 'TRUE'";
 
-            // var customerQuery = @"SELECT
-            // CLIENTE.NOMBRE, CLIENTE.PRIMER_APELLIDO,
-            // CLIENTE.SEGUNDO_APELLIDO
-            // FROM CLIENTE
-            // WHERE CLIENTE.CEDULA_CLIENTE = @id";
+            var customerQuery = @"SELECT
+            CLIENTE.NOMBRE, CLIENTE.PRIMER_APELLIDO,
+            CLIENTE.SEGUNDO_APELLIDO
+            FROM CLIENTE
+            WHERE CLIENTE.CEDULA_CLIENTE = @id";
 
+            var customerData = GetDataById(customerQuery, cedula);
             var washData = GetDataById(washQuery,cedula);
+            var nombre_cliente = (string)customerData.Rows[0]["NOMBRE"];
+            var primer_apellido_cliente = (string)customerData.Rows[0]["PRIMER_APELLIDO"];
+            var segundo_apellido_cliente = (string)customerData.Rows[0]["SEGUNDO_APELLIDO"];
+
             if(washData.Rows.Count != 0)
             {
                 
-                DataTable distinct = washData.DefaultView.ToTable(true,"NOMBRE_LAVADO");
-
+                washData.DefaultView.Sort = "NOMBRE_LAVADO";
+                washData = washData.DefaultView.ToTable();
+                BuildWashReport(nombre_cliente,primer_apellido_cliente,segundo_apellido_cliente,cedula,washData);
             }
             
         }
 
+        private void BuildWashReport(string nombre_cliente, string primer_apellido_cliente, string segundo_apellido_cliente, string cedula, DataTable washData)
+        {
+            HtmlDocument ReportDoc = new HtmlDocument();
+            ReportDoc.Load(@"Data/File Generation/Templates/lavados_cliente.html");
+            var nombre = ReportDoc.GetElementbyId("nombre");
+            nombre.InnerHtml = nombre_cliente + " " +primer_apellido_cliente + " " + segundo_apellido_cliente;
+            var textCedula = ReportDoc.GetElementbyId("cedula");
+            textCedula.InnerHtml = cedula;
+            var fecha = ReportDoc.GetElementbyId("fecha");
+            fecha.InnerHtml = DateTime.Today.ToShortDateString();
+            var htmlTable = ReportDoc.GetElementbyId("table-body");
+            var apparitions = washData.AsEnumerable()
+                                  .GroupBy(e => new { Name = e.Field<string>("NOMBRE_LAVADO")})
+                                  .Select(group => new
+                                  {
+                                    Name = group.Key.Name,
+                                    Count = group.Count()
+                                  });
+
+            foreach (var lavado in apparitions)
+            {
+                htmlTable.InnerHtml = htmlTable.InnerHtml+
+                $"<tr><td><span class=\"text-inverse\">{lavado.Name}</span><br></td><td class=\"text-right\">{lavado.Count}</td></tr>";
+            }
+
+            ReportDoc.Save(@"Data/File Generation/ReportDoc.html");
+            var Renderer = new ChromePdfRenderer();
+            var pdf = Renderer.RenderHtmlFileAsPdf("Data/File Generation/ReportDoc.html");
+            pdf.SaveAs("Data/File Generation/Generated/Reports/Reporte-lavados" + cedula + ".pdf");
+            File.Delete("Data/File Generation/ReportDoc.html");
+
+        }
+
         private void PayrollReport()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Not implemented");
         }
 
         private DataTable GetDataById(string query, string id)
